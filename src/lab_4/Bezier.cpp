2a767d4159f99878fc788693f6b1a8ee3a98e27e
cliche <745356675@qq.com>
2021-11-10 22:32:37 +0800

    -

---
+++ src/lab_4/Bezier.cpp
@@ -0,0 +1,313 @@
+//
+//  Bezier.cpp
+//  lab_4
+//
+//  Created by abc_mac on 2021/11/5.
+//
+
+#include "Bezier.hpp"
+#include <iostream>
+using namespace std;
+
+enum {  // 当前绘制曲线类型
+    CLEAR, BEZIER, BSPLINE
+} currentCurve = CLEAR;
+
+enum {  // 当前对控制点的操作类型
+    IDLE, INSERT, MOVE, REMOVE
+} currentStatus = IDLE;
+
+// global variables
+vector<Point> controlPoints;
+auto movingPoint = controlPoints.end();
+auto inRangePoint = controlPoints.end();
+/*
+int indexOfMovingPoint = -1;
+int indexOfInRangePoint = -1;
+*/
+Color colorOfBezierLine, colorOfBspline, colorOfControlLine;
+
+static void initializer(void);
+static void displayCallback(void);
+static void menuCallback(int value);
+static void mouseCallback(int button, int state, int x, int y);
+static void mouseMotionCallback(int x, int y);
+static void mousePassiveMotionCallback(int x, int y);
+static void keyboardCallback(unsigned char key, int x, int y);
+static void reshapeCallback(int width, int height);
+// deCasteljau
+static void deCasteljau();
+// deBoor
+static void deBoor();
+// draw one pixel
+static void drawPixel(GLfloat x, GLfloat y);
+// 绘制控制点和线
+static void drawControlLine();
+
+int main(int argc, char *argv[]) {
+    glutInit(&argc, argv);
+    // 颜色: RGB显示, 刷新: 双缓存
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    // 窗口位置
+    glutInitWindowPosition(SCR_WIDTH / 2, SCR_HEIGHT / 2);
+    glutInitWindowSize(SCR_WIDTH, SCR_HEIGHT);
+    // 创建窗口, 设置标题
+    glutCreateWindow("Lab_3: Scan And Fill");
+    // 初始化
+    initializer();
+
+    glutCreateMenu(menuCallback);
+    glutAddMenuEntry("Beizer", 1);
+    glutAddMenuEntry("B-spline", 2);
+    glutAddMenuEntry("Insert", 3);
+    glutAddMenuEntry("Move", 4);
+    glutAddMenuEntry("Remove", 5);
+    glutAddMenuEntry("ClearScreen", 6);
+    glutAddMenuEntry("Exit", 7);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
+    glutMouseFunc(mouseCallback);
+    glutMotionFunc(mouseMotionCallback);
+    glutPassiveMotionFunc(mousePassiveMotionCallback);
+    glutKeyboardFunc(keyboardCallback);
+    // 画图展示函数
+    glutDisplayFunc(displayCallback);
+    glutReshapeFunc(reshapeCallback);
+    
+    glutMainLoop();
+    
+    return 0;
+}
+
+void initializer(void) {
+    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glPointSize(3.0f);
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0, SCR_WIDTH, 0, SCR_HEIGHT);
+}
+
+void displayCallback(void) {
+    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glPointSize(3.0f);
+    glLineWidth(3.0f);
+    // 绘制控制点和对应的连线
+    drawControlLine();
+    // 绘制对应的曲线
+    if (currentCurve == BEZIER)
+        deCasteljau();
+    else if (currentCurve == BSPLINE)
+        deBoor();
+    // 交换缓冲区
+    glutSwapBuffers();
+}
+
+void menuCallback(int value) {
+    switch (value) {
+        case 1: {
+            currentCurve = BEZIER;
+            currentStatus = IDLE;
+            break;
+        }
+        case 2: {
+            currentCurve = BSPLINE;
+            currentStatus = IDLE;
+            break;
+        }
+        case 3:
+            currentStatus = INSERT;
+            break;
+        case 4:
+            currentStatus = MOVE;
+            break;
+        case 5:
+            currentStatus = REMOVE;
+            break;
+        case 6: {
+            glClear(GL_COLOR_BUFFER_BIT);
+            glutSwapBuffers();
+            currentCurve = CLEAR;
+            currentStatus = IDLE;
+            controlPoints.clear();
+            movingPoint = controlPoints.end();
+            inRangePoint = controlPoints.end();
+            break;
+        }
+        default:
+            exit(0);
+    }
+}
+
+void mouseCallback(int button, int state, int x, int y) {
+    y = SCR_HEIGHT - y;
+    
+    if (button == GLUT_LEFT_BUTTON) {
+        if (state == GLUT_DOWN) {
+            switch (currentStatus) {
+                case INSERT: {
+                    controlPoints.emplace_back(x, y);
+                    cout << "插入控制点: [" << x << ", " << y << "]\n";
+                    glutPostRedisplay();
+                    break;
+                }
+                case MOVE: {
+                    if (inRangePoint != controlPoints.end()) {
+                        movingPoint = inRangePoint;
+                    }
+                    /*
+                    if (indexOfInRangePoint != -1) {
+                        indexOfMovingPoint = indexOfInRangePoint;
+                    }
+                     */
+                    break;
+                }
+                case REMOVE: {
+                    if (inRangePoint != controlPoints.end()) {
+                        cout << "删除控制点: [" << inRangePoint->x << ", " << inRangePoint->y << "]\n";
+                        controlPoints.erase(inRangePoint);
+                        inRangePoint = controlPoints.end();
+                        glutPostRedisplay();
+                    }
+                    /*
+                    if (indexOfInRangePoint != -1) {
+                        cout << "删除控制点: [" << controlPoints[indexOfInRangePoint].x << ", " << controlPoints[indexOfInRangePoint].y << "]\n";
+                        controlPoints.erase(controlPoints.begin() + indexOfInRangePoint);
+                        indexOfInRangePoint = -1;
+                    }
+                     */
+                    break;
+                }
+                default:
+                    break;
+            }
+        } else if (state == GLUT_UP && currentStatus == MOVE) {
+            movingPoint = controlPoints.end();
+        }
+    }
+    cout << "Button_Left = [" << x << ", " << y << "]\n";
+    glutPostRedisplay();
+}
+
+void mouseMotionCallback(int x, int y) {
+    y = SCR_HEIGHT - y;
+    
+    if (currentStatus == MOVE) {
+        if (movingPoint != controlPoints.end()) {
+            movingPoint->moveTo(x, y);
+            glutPostRedisplay();
+        }
+    }
+}
+
+void mousePassiveMotionCallback(int x, int y) {
+    y = SCR_HEIGHT - y;
+    
+    bool handMouseOn = false;
+    if (currentStatus == MOVE || currentStatus == REMOVE) {
+        // MOVE/REMOVE时, 鼠标接近某个控制点时, 自动变为手指
+        for (auto iter = controlPoints.begin(); iter != controlPoints.end(); ++iter) {
+            if (iter->isInControlRange(x, y)) {
+                handMouseOn = true;
+                inRangePoint = iter;
+                break;
+            }
+        }
+        if (handMouseOn)
+            glutSetCursor(GLUT_CURSOR_INFO);
+        else
+            glutSetCursor(GLUT_CURSOR_INHERIT);
+    }
+}
+
+void keyboardCallback(unsigned char key, int x, int y) {
+    switch (key) {
+        case '0': {
+            currentCurve = BEZIER;
+            currentStatus = IDLE;
+            break;
+        }
+        case '1': {
+            currentCurve = BSPLINE;
+            currentStatus = IDLE;
+            break;
+        }
+        case 'i':
+            currentStatus = INSERT;
+            break;
+        case 'm':
+            currentStatus = MOVE;
+            break;
+        case 'r':
+            currentStatus = REMOVE;
+            break;
+        case 'c': {
+            glClear(GL_COLOR_BUFFER_BIT);
+            glutSwapBuffers();
+            currentCurve = CLEAR;
+            currentStatus = IDLE;
+            break;
+        }
+        case GLUT_ESCAPE:
+            exit(0);
+    }
+}
+
+void reshapeCallback(int width, int height) {
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glViewport(0, 0, width, height);
+    gluOrtho2D(0, width, 0, height);
+}
+
+void deCasteljau() {
+    if (controlPoints.size() < 2)
+        return;
+    
+    vector<Point> currentPoints(controlPoints);
+    vector<Point> bezierPoints;
+    unsigned long n = controlPoints.size();
+    
+    for (float t = 0.0f; t <= 1.0f; t += 0.005f / n) {
+        for (int i = 1; i < n; i++) {
+            // 递推, 逐渐缩小控制点规模, 每走一轮，控制点个数 - 1
+            for (int j = 0; j < n - i; j++) {
+                // 在当前控制点规模下绘制曲线
+                if (i == 1) {
+                    // 第一轮必须是最初的控制点, 需要用到controlPoints
+                    currentPoints[j].x = (1 - t) * controlPoints[j].x + t * controlPoints[j + 1].x;
+                    currentPoints[j].y = (1 - t) * controlPoints[j].y + t * controlPoints[j + 1].y;
+                    continue;
+                } else {
+                    // 递推, 每次都用上一轮的点更新当前点, 直到推到贝塞尔曲线上的点
+                    currentPoints[j].x = (1 - t) * currentPoints[j].x + t * currentPoints[j + 1].x;
+                    currentPoints[j].y = (1 - t) * currentPoints[j].y + t * currentPoints[j + 1].y;
+                }
+            }
+        }
+        // cout << "控制点数量=" << controlpoint.size() << endl;一直是点的数目，因为之前直接存在这个里面了，但是有用的只有下标为1的才有用
+        // 递推完成, currentPoints[0]中存放的是贝塞尔曲线上的点
+        bezierPoints.emplace_back(currentPoints[0]);
+    }
+    
+    glBegin(GL_LINE_STRIP);
+    glColor3f(colorOfBezierLine.r, colorOfBezierLine.g, colorOfBezierLine.b);
+    for (auto &p : bezierPoints)
+        drawPixel(p.x, p.y);
+    glEnd();
+}
+
+void deBoor() {
+    
+}
+
+void drawPixel(GLfloat x, GLfloat y) {
+    glVertex2f(x, y);
+}
+
+void drawControlLine() {
+    glBegin(GL_LINE_STRIP);
+    glColor3f(colorOfControlLine.r, colorOfControlLine.g, colorOfControlLine.b);
+    for (auto &p : controlPoints)
+        drawPixel(p.x, p.y);
+    glEnd();
+}
---
+++ src/lab_4/Bezier.hpp
@@ -0,0 +1,70 @@
+//
+//  Bezier.hpp
+//  lab_4
+//
+//  Created by abc_mac on 2021/11/5.
+//
+
+#ifndef Bezier_hpp
+#define Bezier_hpp
+
+#define GL_SILENCE_DEPRECATION
+#define GLUT_ESCAPE 27
+#define SCR_WIDTH 800
+#define SCR_HEIGHT 600
+#include <GLUT/glut.h>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+struct Color {
+    GLfloat r;
+    GLfloat g;
+    GLfloat b;
+    
+    Color() {
+        this->r = static_cast<GLfloat>(rand()) / static_cast<GLfloat>(RAND_MAX);
+        this->g = static_cast<GLfloat>(rand()) / static_cast<GLfloat>(RAND_MAX);
+        this->b = static_cast<GLfloat>(rand()) / static_cast<GLfloat>(RAND_MAX);
+    }
+    Color(const GLfloat r, const GLfloat g, const GLfloat b): r(r), g(g), b(b) {}
+    Color(const Color &color) {
+        this->r = color.r;
+        this->g = color.g;
+        this->b = color.b;
+    }
+    Color operator * (const GLfloat times) {
+        return Color(r * times, g * times, b * times);
+    }
+};
+
+struct Point {
+    float x;
+    float y;
+    float z;
+    float controlRadius;
+    Point(float x = 0, float y = 0, float z = 0) {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+        this->controlRadius = 7.0;
+    }
+    Point(const Point &p) {
+        this->x = p.x;
+        this->y = p.y;
+        this->z = p.z;
+        this->controlRadius = p.controlRadius;
+    }
+    
+    bool isInControlRange(float x, float y) {
+        return sqrt(pow(x - this->x, 2) + pow(y - this->y, 2)) < this->controlRadius;
+    }
+    
+    void moveTo(float x, float y) {
+        this->x = x;
+        this->y = y;
+    }
+};
+
+
+#endif /* Bezier_hpp */
